71da92070674da8e4937b015848ca70d8f27ed8a
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-04-01 20:51:49 +0100

    part 1 c stateful component

---
+++ part1/part1.c/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = (props) => {
+  const {counter} = props
+  return (
+    <div>{counter}</div>
+  )
+}
+
+export default App
---
+++ part1/part1.c/src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />, 
+  document.getElementById('root'))
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
