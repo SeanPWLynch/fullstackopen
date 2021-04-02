a22aa8c5b7b2f2dafe380566d0584385c7e466e1
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-04-02 12:31:39 +0100

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,41 +6,52 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () =>
-  {
-    setGood(good+1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
   }
 
-  const handleNeutralClick = () =>
-  {
-    setNeutral(neutral+1)
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
   }
 
-  const handleBadClick = () =>
-  {
-    setBad(bad+1)
+  const handleBadClick = () => {
+    setBad(bad + 1)
   }
+
+  const total = () =>
+    (good + neutral + bad)
+
+
+  const average = () =>
+    ((good - bad) / total())
+
+
+  const positive = () =>
+    (good / total()) * 100
+
 
   return (
     <div>
       <div><h1>Give Feedback</h1></div>
       <div>
-        <Button handleClick={handleGoodClick} text='Good'/>
-        <Button handleClick={handleNeutralClick} text='Neutral'/>
-        <Button handleClick={handleBadClick} text='Bad'/>
+        <Button handleClick={handleGoodClick} text='Good' />
+        <Button handleClick={handleNeutralClick} text='Neutral' />
+        <Button handleClick={handleBadClick} text='Bad' />
       </div>
       <div><h1>Statistics</h1></div>
       <div><p>Good {good}</p></div>
       <div><p>Neutral {neutral}</p></div>
       <div><p>Bad {bad}</p></div>
+      <div><p>All {total()}</p></div>
+      <div><p>Average {average()}</p></div>
+      <div><p>Positive {positive()}</p></div>
     </div>
   )
 }
 
-const Button = ({handleClick, text}) =>
-{
-  return(
-  <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
   )
 }
 
