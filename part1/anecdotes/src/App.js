0de764e424328ed203ee72f79866b484c2f6122b
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-04-03 09:27:59 +0100

    1.14*: anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -11,25 +11,66 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0
+  });
+
   const [selected, setSelected] = useState(0)
 
-  const randomAnecdote = (props) => {
-    console.log('Current ', props.selected)
+  const [mostVotes, setMostVotes] = useState({
+    anedcote: anecdotes[0],
+    votes: 0
+  })
+
+  const randomAnecdote = () => {
+    console.log('Current ', selected)
     let nextIndex = 0
-    while ((nextIndex = Math.floor(Math.random() * anecdotes.length)) === props.selected) {
-      console.log('Finding Next ', props.selected)
+    while ((nextIndex = Math.floor(Math.random() * anecdotes.length)) === selected) {
+      console.log('Finding Next ', selected)
     }
     setSelected(nextIndex)
+  }
+
+  const vote = () => {
+    const pointcopy = { ...points }
+    pointcopy[selected]++
+    setPoints(pointcopy)
+    if (pointcopy[selected] > mostVotes.votes) {
+      console.log(anecdotes[selected], 'Now Most Voted')
+      const mostVotesCopy = {
+        anecdote: anecdotes[selected],
+        votes: pointcopy[selected]
+      }
+      console.log(mostVotesCopy)
+      setMostVotes(mostVotesCopy)
+    }
   }
 
   return (
     <div>
       <div>
-        {anecdotes[selected]}
+        <h1>Anecdote Of The Day</h1>
       </div>
       <div>
+        {anecdotes[selected]}
+      </div>
+      <div>Has {points[selected]} Votes</div>
+      <div>
+        <button onClick={vote}>Vote</button>
         <button onClick={() => randomAnecdote({ selected })}>Next Anecdote</button>
       </div>
+      <div>
+        <h1>Most Voted</h1>
+      </div>
+      <div>
+        {mostVotes.anecdote}
+      </div>
+      <div>Has {mostVotes.votes} Votes</div>
     </div>
   )
 }
