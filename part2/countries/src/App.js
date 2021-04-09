9fa7bcee80354bfb4f70061bf92f88ec68c13ca6
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-04-09 21:53:32 +0100

    2.12* Data for countries, step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Filter = ({ changeHandler, filter }) => {
+
+  return (
+    <div>
+      Find Countries: <input onChange={changeHandler} value={filter} />
+    </div>
+  )
+}
+
+const Countries = ({ countryList, countryFilter }) => {
+
+  if (countryList.length > 9) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+  else if (countryList.length > 1 && countryList.length < 8) {
+    return (
+      <div>
+        {countryList.map(country => <p>{country.name}</p>)}
+      </div>
+    )
+  }
+  else if (countryList.length === 1) {
+    return (
+      <div>
+        <Country country={countryList[0]} />
+      </div>
+    )
+  }
+  else {
+    return (
+      <p>No match found for: {countryFilter}</p>
+    )
+  }
+}
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <h3>Languages</h3>
+      <ul>{country.languages.map(language => <li key={language.name}>{language.name}</li>)}</ul>
+      <h3>Flag</h3>
+      <img src={country.flag} width="250px" alt="Country Flag"></img>
+    </div>
+  )
+
+}
+
+const App = () => {
+
+  const getCountries = () => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('API Response: ', response.data);
+        setCountries(response.data)
+      })
+  }
+
+  useEffect(getCountries, [])
+
+
+  const [countries, setCountries] = useState([])
+  const [countryFilter, setCountryFilter] = useState('')
+
+  const handleCountryNameFilter = (event) => {
+    setCountryFilter(event.target.value)
+  }
+
+  const countriesToShow = countryFilter === '' ? countries : countries.filter(country => country.name.toUpperCase().includes(countryFilter.toUpperCase()))
+
+  return (
+    <div>
+      <Filter changeHandler={handleCountryNameFilter} value={countryFilter} />
+      <Countries countryList={countriesToShow} countryFilter={countryFilter} />
+    </div>
+  )
+}
+
+export default App
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Person from './Person';
 
 const Persons = ({ personList }) => {
-
   return (
     <ul>
       {personList.map(person => <Person key={person.name} person={person} />)}
