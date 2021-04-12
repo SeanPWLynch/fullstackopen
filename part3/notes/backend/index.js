09e917054cd4b98a2c6783078d381838565790f6
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-04-12 19:29:39 +0100

    sync

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -93,7 +95,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on post ${PORT}`)
 })
