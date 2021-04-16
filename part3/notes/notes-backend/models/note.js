b8ade671662bc688bc38152f924972fa1b17e3a9
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-04-16 14:20:51 +0100

    refactor notes

--- part3/notes/notes-backend/models/note.js
+++ part3/notes/notes-backend/models/note.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require("dotenv").config();
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
@@ -17,10 +18,12 @@ const noteSchema = new mongoose.Schema({
     important: Boolean
 })
 
-noteSchema.set('toJson',{
-    transform: (document, returnedObject) =>{
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject__v
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
     }
-})
+  })
+
+module.exports = mongoose.model('Note', noteSchema)
