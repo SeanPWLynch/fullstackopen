4addcf31fe12e62c1bfb108ed833bf9eccd59af6
Sean Lynch <sean.lynch@gmail.com>
2021-04-25 12:16:28 +0100

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/bloglist-backend/utils/list_helper.js
+++ part4/bloglist/bloglist-backend/utils/list_helper.js
@@ -6,11 +6,23 @@ const totalLikes = (blogs) => {
     const reducer = (sum, item) => {
         return sum + item.likes
     }
+
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+
+}
+
+const favouriteBlog = (blogs) => {
+    const reducer = (prev, current) => {
+
+        return (prev.likes > current.likes) ? prev : current 
+    }
+
+    return blogs.length === 0 ? 0 : blogs.reduce(reducer, blogs[0])
 
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
