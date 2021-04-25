e5d6a61ca871194c0e5a750866550e0d55424c5e
Sean Lynch <sean.lynch@gmail.com>
2021-04-25 16:52:46 +0100

    4.8: Blog list tests, step1

---
+++ part4/bloglist/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('../utils/test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
