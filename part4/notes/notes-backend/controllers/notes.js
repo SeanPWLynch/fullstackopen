7475bb468b67fdce25c43d1cc8b9e94935203087
Sean Lynch <sean.lynch@gmail.com>
2021-04-25 16:17:50 +0100

    Optimizing the beforeEach function

--- part4/notes/notes-backend/controllers/notes.js
+++ part4/notes/notes-backend/controllers/notes.js
@@ -7,16 +7,14 @@ notesRouter.get('/', async (request, response) => {
 })
 
 notesRouter.get('/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch (exception) {
-    next(exception)
+
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
+
 })
 
 notesRouter.post('/', async (request, response, next) => {
@@ -27,22 +25,17 @@ notesRouter.post('/', async (request, response, next) => {
     important: body.important || false,
     date: new Date(),
   })
-  try {
-    const savedNote = await note.save()
-    response.json(savedNote)
-  } catch (exception) {
-    next(exception)
-  }
+
+  const savedNote = await note.save()
+  response.json(savedNote)
+
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    next(exception)
-  }
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
+
 
 notesRouter.put('/:id', (request, response, next) => {
   const body = request.body
