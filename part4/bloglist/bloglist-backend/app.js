04d8345b638bf1cfbe30b6d051a1ba35bcd321bf
Sean Lynch <sean.lynch@gmail.com>
2021-04-25 13:10:28 +0100

    adding extra utils to blogpost app

--- part4/bloglist/bloglist-backend/app.js
+++ part4/bloglist/bloglist-backend/app.js
@@ -1,3 +1,4 @@
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -6,6 +7,9 @@ const mongoose = require('mongoose')
 require("dotenv").config();
 const Blog = require('./models/blog');
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
 
 const mongoUrl = process.env.MONGODB_URI
 
@@ -13,7 +17,10 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
