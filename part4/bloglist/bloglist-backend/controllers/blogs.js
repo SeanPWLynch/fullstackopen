522cf297c5e1e2f57e1f72b74463668477e74799
Sean Lynch <sean.lynch@gmail.com>
2021-05-05 19:14:04 +0100

    4.19: bloglist expansion, step7

--- part4/bloglist/bloglist-backend/controllers/blogs.js
+++ part4/bloglist/bloglist-backend/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const { json } = require('express');
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
 
 blogsRouter.get('/', async (request, response, next) => {
     const blogs = await Blog.find().populate('user', {username: 1, name: 1, id: 1});
@@ -18,11 +27,15 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-
-
     const body = new Blog(request.body)
 
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
