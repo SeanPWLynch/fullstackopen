8caf416efcfeba8edf865a33e29245b3e83d48f5
Sean Lynch <sean.lynch@gmail.com>
2021-05-05 19:10:09 +0100

    4.18: bloglist expansion, step6

--- part4/bloglist/bloglist-backend/app.js
+++ part4/bloglist/bloglist-backend/app.js
@@ -11,6 +11,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const loginRouter = require('./controllers/login')
 
 
 const mongoUrl = process.env.MONGODB_URI
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
